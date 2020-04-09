2c49dc230936f5c1e8274472d6c2b71e47a1add8
Guillaume RISCHARD <git@stereo.lu>
2020-04-09 15:45:44 +0200

    Check number of localites; format; add debug code

--- sources/luxembourg_addresses.py
+++ sources/luxembourg_addresses.py
@@ -3,20 +3,30 @@
 import requests
 import csv
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
 def get():
     # The endpoint that redirects to the most recent version of the
     # addresses in csv.
-    ADDRESSES_CSV = 'https://data.public.lu/fr/datasets/r/5cadc5b8-6a7d-4283-87bc-f9e58dd771f7'
+    ADDRESSES_CSV = (
+        "https://data.public.lu/fr/datasets/r/5cadc5b8-6a7d-4283-87bc-f9e58dd771f7"
+    )
     # Downloading the addresses might take ~15 seconds.
     # In the meanwile, shake your wrists and correct your posture.
     r = requests.get(ADDRESSES_CSV)
-    r.encoding = 'utf-8'
+    r.encoding = "utf-8"
     req_addresses = r.text.splitlines()
-    csvreader = csv.DictReader(req_addresses, delimiter=';')
+    csvreader = csv.DictReader(req_addresses, delimiter=";")
     fieldnames = csvreader.fieldnames
-    addresses = [{k: v for k, v in row.items()}
-        for row in csvreader]
+    addresses = [{k: v for k, v in row.items()} for row in csvreader]
     for row in addresses:
-        row['code_commune'] = row['id_geoportail'][:3]
-    fieldnames.insert(0, 'code_commune')
+        row["code_commune"] = row["id_geoportail"][:3]
+    fieldnames.insert(0, "code_commune")
+    localites = list(set(x["localite"] for x in addresses))
+    log.debug("Localites found: %s. Expecting 542.", str(len(localites)))
+    if len(localites) != 542:
+        raise IOError(f"Localites found: {len(localites)}. Expected 542!")
     return addresses, fieldnames
