80e7b08294a1ce99bf113bd248d8b0e317c96989
Guillaume RISCHARD <git@stereo.lu>
2021-02-25 17:43:39 +0100

    Update run.sh to import to postgis if possible

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+# TODO https://stackoverflow.com/questions/41696675/how-to-copy-a-csv-file-from-a-url-to-postgresql
 source venv/bin/activate
 echo "Addresses..."
 ./csventrifuge.py luxembourg_addresses luxembourg-addresses.csv
 echo "Streets..."
 ./csventrifuge.py luxembourg-caclr-dicacolo luxembourg-streets.csv
-echo "Importing addresses into postgres..."
-psql -d gis -f ../addresses/import_addresses.sql
-echo "Copying to openstreetmap.lu..."
-scp luxembourg-addresses.csv luxembourg-streets.csv root@stereo.lu:/home/openstreetmap/www/
+if ! command -v psql &> /dev/null
+then
+    echo "Importing addresses into postgres..."
+    psql -d gis -f ../addresses/import_addresses.sql
+    psql -d gis -f ../import_cadastre.sql
+fi
+echo "Converting for JOSM"
+echo addr:street,addr:housenumber,addr:city,addr:postcode,ref:caclr,lat_wgs84,lon_wgs84,commune > "$(date +%Y-%m-%d)-addresses.csv"
+cut -d , -f 2-5,7-9 luxembourg-addresses.csv | tail -n +2 >> "$(date +%Y-%m-%d)-addresses.csv"
+rm latest-addresses.csv
+ln -s "$(date +%Y-%m-%d)-addresses.csv" latest-addresses.csv
+echo "Done! To open, copy-paste:"
+echo "open -a JOSM.app $(date +%Y-%m-%d)-addresses.csv"
