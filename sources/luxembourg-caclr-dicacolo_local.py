15051ddf5e6a0f89b8b1722c9d912f4f36e1096f
Guillaume RISCHARD <git@stereo.lu>
2022-11-29 17:55:20 +0100

    Expand ~ in path

--- sources/luxembourg-caclr-dicacolo_local.py
+++ sources/luxembourg-caclr-dicacolo_local.py
@@ -2,27 +2,15 @@
 # Call with get(), you get a collection containing dicts, and the field names list. That's the deal.
 
 from io import TextIOWrapper
-
-# from zipfile import ZipFile
-# import requests
-
+import os
 
 def trimget(data, startpos, length):
     return data[int(startpos) : int(startpos) + int(length)].rstrip(" ")
 
 
 def get():
-    # The endpoint that redirects to the most recent version of the
-    # CACLR in zip.
-    # CACLR_ZIP = 'https://data.public.lu/fr/datasets/r/af76a119-2bd1-462c-a5bf-23e11ccfd3ee'
-
-    # Downloading the CACLR might take ~15 seconds.
-    # In the meanwile, shake your wrists and correct your posture.
-    # r = requests.get(CACLR_ZIP)
-    # zipfile = ZipFile(BytesIO(r.content))
-    # zip_names = zipfile.namelist()
     caclr = []
-    with open("~/caclr/TR.DICACOLO.RUCP", "r") as extracted_file:
+    with open(os.path.expanduser("~/caclr/TR.DICACOLO.RUCP", "r")) as extracted_file:
         for data in TextIOWrapper(extracted_file, "latin-1"):
             caclr.append(
                 {
