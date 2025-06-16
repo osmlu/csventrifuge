fd855ac0670c3f564405af707af075bfb546cab5
Guillaume Rischard <github@stereo.lu>
2025-06-16 12:26:39 -0400

    Ensure all source modules return DataFrame

--- sources/luxembourg-caclr-localites_local.py
+++ sources/luxembourg-caclr-localites_local.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # Call with get(), you get a collection containing dicts, and the field names list. That's the deal.
 
-from io import BytesIO, TextIOWrapper
-from zipfile import ZipFile
-import requests
 import os
+import polars as pl
 
 def trimget(data, startpos, length):
     return data[int(startpos):int(startpos)+int(length)].rstrip(' ')
 
-def get():
+def get() -> pl.DataFrame:
+    """Return localities as a ``polars.DataFrame``."""
     caclr = []
-    with open(os.path.expanduser('~/caclr/LOCALITE'), 'r', encoding='ISO-8859-15') as extracted_file:
+    with open(
+        os.path.expanduser('~/caclr/LOCALITE'),
+        'r',
+        encoding='ISO-8859-15'
+    ) as extracted_file:
         for data in extracted_file:
             caclr.append({
                 'numero': trimget(data, 0,  5),
@@ -21,9 +24,10 @@ def get():
                 'date_fin_valid': trimget(data, 87, 10),
                 'ds_timestamp_modif': trimget(data, 99,10),
                 'fk_canto_code': trimget(data, 109,2),
-                'fk_commu_code': trimget(data, 112, 2)
+                'fk_commu_code': trimget(data, 112, 2),
             })
-        return caclr, list(caclr[0].keys())
+    df = pl.DataFrame(caclr).with_columns(pl.all().cast(pl.String))
+    return df
 
 if __name__ == "__main__":
     print(get())
