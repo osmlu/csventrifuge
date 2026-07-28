97c214b8bdd5982ccd9b08c73a5770df34bf13d2
Guillaume RISCHARD <git@stereo.lu>
2026-07-28 15:46:56 -0400

    Updated run script to run with shebang

--- run.sh
+++ run.sh
@@ -2,13 +2,12 @@
 # TODO https://stackoverflow.com/questions/41696675/how-to-copy-a-csv-file-from-a-url-to-postgresql
 #source venv/bin/activate
 echo "Addresses..."
-python3 csventrifuge.py luxembourg_addresses luxembourg-addresses.csv
+./csventrifuge.py luxembourg_addresses luxembourg-addresses.csv
 echo "Streets..."
-python3 ./csventrifuge.py luxembourg-caclr-dicacolo_local luxembourg-streets.csv
+./csventrifuge.py luxembourg-caclr-dicacolo_local luxembourg-streets.csv
 echo "CACLR..."
-python3 ./csventrifuge.py luxembourg-caclr-commuall_local luxembourg-communes.csv
-if command -v psql &> /dev/null
-then
+./csventrifuge.py luxembourg-caclr-commuall_local luxembourg-communes.csv
+if command -v psql &> /dev/null; then
     # TODO https://stackoverflow.com/questions/41696675/how-to-copy-a-csv-file-from-a-url-to-postgresql
     echo "Importing addresses into postgres..."
     psql -d osmlu -f ../import_addresses.sql
@@ -19,4 +18,4 @@ cp luxembourg-streets.csv ../public_html/csventrifuge/.
 echo "Converting for JOSM"
 #echo addr:street,addr:housenumber,addr:city,addr:postcode,ref:caclr,lat_wgs84,lon_wgs84,commune > "$(date +%Y-%m-%d)-addresses.csv"
 #cut -d , -f 3-6,8-10 luxembourg-addresses.csv | tail -n +2 >> "$(date +%Y-%m-%d)-addresses.csv"
-python3 process_addresses.py
+./process_addresses.py
